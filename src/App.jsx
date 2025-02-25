372d79d164c7e9257262dbf5e0fbd63418cfc01b
Matheus Nobrega <matheus.adao.nobrega@gmail.com>
2025-02-25 15:04:11 -0300

    atualizacao dos botoes

--- src/App.jsx
+++ src/App.jsx
@@ -4,23 +4,41 @@ import Carta1 from "./imagens/carta1.png";
 import Carta2 from "./imagens/carta2.png";
 
 function ContadorTruco() {
+  // Pontuações fixas
+  const pontuacaoTimeA = 6;
+  const pontuacaoTimeB = 9;
 
   return (
     <div className="contadorTruco">
       <h1>Contador de Truco</h1>
       <p>
-        "Time A Venceu!"
+        {pontuacaoTimeA === 12
+          ? "Time A Venceu!"
+          : pontuacaoTimeB === 12
+            ? "Time B Venceu!"
+            : null}
       </p>
       <div className="timesContainer">
         <div className="timeCard">
-          <h2>Time A: 12</h2>
+          <h2>Time A: {pontuacaoTimeA}</h2>
           <img src={Carta1} alt="Carta Time A" />
+          <div className="buttonGroup">
+            <button className="botaoCard" disabled>+1</button>
+            <button className="botaoCard" disabled>+3</button>
+            <button className="botaoCard" disabled>-1</button>
+          </div>
         </div>
         <div className="timeCard">
-          <h2>Time B: 0</h2>
+          <h2>Time B: {pontuacaoTimeB}</h2>
           <img src={Carta2} alt="Carta Time B" />
+          <div className="buttonGroup">
+            <button className="botaoCard" disabled>+1</button>
+            <button className="botaoCard" disabled>+3</button>
+            <button className="botaoCard" disabled>-1</button>
+          </div>
         </div>
       </div>
+      <button className="resetButton" disabled>Resetar Pontuação</button>
     </div>
   );
 }
